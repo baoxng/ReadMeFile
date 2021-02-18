1c2077a0fa2a7c745b5f0685356cf637fe067ada
Bao Xiong <bao.xiong2011@gmail.com>
2021-02-18 14:15:58 -0600

    Added first prompt for readme file..

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+//Packages
 const inquirer = require('inquirer');
 const fs= require ('fs');
-const generateMarkdown = require('');
+const generateMarkdown = require('./utilities/generateMarkdown');
 
+//Prompts and questions
+const prompts = [
+    {
+        type: 'input',
+        message: "",
+        name: '',
+        default: '',
+        validate: function(answer) {
+            if (answer.length < 1){
+                return console.log('Must have a valid GitHub username.')
+            }
+            return true; 
+            }
+    },
+    {
 
+    }   
+]
 
