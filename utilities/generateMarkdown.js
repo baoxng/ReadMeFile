7a37317b2cd67868c6d9e05fcbe1fe3446a44505
Bao Xiong <bao.xiong2011@gmail.com>
2021-02-18 22:46:38 -0600

    Repositioned badges.

--- utilities/generateMarkdown.js
+++ utilities/generateMarkdown.js
@@ -21,7 +21,9 @@ function generateMarkdown(userResponse, userInfo) {
     // Generate markdown for the top required portions of the README
     let writeMarkdown = 
     `# ${userResponse.title}
-    ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/${userResponse.username}/${userResponse.repo}?style=flat&logo=appveyor) ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${userResponse.username}/${userResponse.repo}?style=flat&logo=appveyor)
+    ![Badge for GitHub repo top language](https://img.shields.io/github/languages/top/${userResponse.username}/${userResponse.repo}?style=flat&logo=appveyor) 
+    
+    ![Badge for GitHub last commit](https://img.shields.io/github/last-commit/${userResponse.username}/${userResponse.repo}?style=flat&logo=appveyor)
     
     Check out the badges hosted by [shields.io](https://shields.io/).
     
